5af8b9414fa63f9c508c9e052bfc8654b7a6cd1e
Adam Capdeville <agcapdeville@gmail.com>
2020-03-20 23:18:44 -0700

    Update Ham

---
+++ README.md
@@ -0,0 +1,5 @@
+# SMS-Spam-Bot
+Python bot for detection of spam in text messages
+
+![GIF](https://media.giphy.com/media/QQ5WeXja47qzm/giphy.gif)
+
--- spam_bot.py
+++ spam_bot.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 
 
-
 textFile = open('./data/SMSSpamCollection','r')
 
 msgType = []
@@ -22,19 +21,19 @@ while line:
     if count % 10 == 0:
         if line.find('spam') >= 0:
             testMsgType.append('S')
-            testMsgBody.append(line[4:len(line)])
+            testMsgBody.append(line[5:len(line)])
         elif line.find('ham') >= 0:
             testMsgType.append('H')
-            testMsgBody.append(line[3:len(line)])
+            testMsgBody.append(line[4:len(line)])
 
     else:
         if line.find('spam') >= 0:
             msgType.append('S')
-            msgBody.append(line[4:len(line)])
+            msgBody.append(line[5:len(line)])
 
         elif line.find('ham') >= 0:
             msgType.append('H')
-            msgBody.append(line[3:len(line)])
+            msgBody.append(line[4:len(line)])
 
     line = textFile.readline()
     count += 1
@@ -42,29 +41,42 @@ while line:
 
 msgData = pd.DataFrame(list(zip(msgType, msgBody)), columns=['MsgType','MsgBody'])
 testMsgData = pd.DataFrame(list(zip(testMsgType, testMsgBody)), columns=['MsgType','MsgBody'])
+
+
 # print(msgData)
 # print(testMsgData)
 
 # Each test set is made up of a message type ( Ham OR Spam) & a message body (" mesgdsfsf sdf sd!.")
 # We want to test each word in testData against each word of each message in messageData to see the probability of it being Ham OR Spam...
 
-#  Algorithm:
+# TODO:
+#  Algorithm: HAM OR SPAM
 # 
 #   alpha = pseudo-count (we are using 0.2)
 #   N = samples (we are using 20000)
 # 
-#                        count(word,ham/spam) + alpha
-#  P(word|ham/spam) =   -------------------------------
-#                        count(ham/spam) + N * alpha
+#                        count(word,ham) + alpha
+#  P(word|ham) =   -------------------------------
+#                        count(ham) + N * alpha
 # 
-#  What is a word?
+#             - - - - < OR > - - - -
+# 
+#                        count(word,spam) + alpha
+#  P(word|spam) =   -------------------------------
+#                        count(spam) + N * alpha
+# 
+# 
+# https://www.kaggle.com/dilip990/spam-ham-detection-using-naive-bayes-classifier
+
+# 
+#  What is a word? Bird is a word. 
 #  Avoid all special characters like : (?;@3$%!) except (I'll) the '
+
+
 
 # def count( word, inType, inData):
 #     count = 0
-
-#     # for i in range(DataFrame):
-    
+#     # for i in range(DataFrame):    
 #     print(inData)
 #     return count
 
@@ -73,20 +85,46 @@ import string
 N = 20000
 alpha = 0.2
 
-msg = testMsgData.loc[0][1]
-print("msg:", msg)
-words = msg.split()
-print("words:", words)
-print('word:', words[0])
-print(string.punctuation)
+# msg = testMsgData.loc[0][1]
+# print("msg:", msg)
+# words = msg.split()
+# print("words:", words)
+# print('word:', words[0])
+# print(string.punctuation)
 
-print(len(testMsgData.loc[0][1].split()))
+# REMOVING PUNCTUATION:
+for i in range(len(msgData)):
+    mess = str(msgData.iloc[i]['MsgBody'])
+    nopunc=[char for char in mess if char not in string.punctuation]
+    nopunc=''.join(nopunc)
+    msgData.iloc[i]['MsgBody'] = nopunc
+
+for i in range(len(testMsgData)):
+    mess = str(msgData.iloc[i]['MsgBody'])
+    nopunc=[char for char in mess if char not in string.punctuation]
+    nopunc=''.join(nopunc)
+    testMsgData.iloc[i]['MsgBody'] = nopunc
+
+
+#  Probability of SPAM/HAM
+# 
+#                 COUNT(HAM MESSAGES) + K
+#   P(HAM) =   ----------------------------
+#                 N(TOTAL) + 1*CLASSES
+
+#  CLASSES = 2, 
+
+
+# Msg DATA HAM & SPAM COUNTS
+groups = msgData.groupby('MsgType')
+hamCount = groups.get_group("H").count().values[0]
+spamCount = groups.get_group("S").count().values[0]
 
 
 
 
-# for each in range(len(testMsgData)):
-#     print(testMsgData.loc[each][1])
 
-#     count(testData,"ham",msgData)
+# print( len(msgData) )
+# print( msgData.iloc[len(msgData)-1][1] )
 
+# print(len(testMsgData))
