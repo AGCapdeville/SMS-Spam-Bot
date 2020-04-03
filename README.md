cfc3a61625a9566f94ecdb66da4fae0d5f0dfcaa
Adam Capdeville <agcapdeville@gmail.com>
2020-04-02 22:59:25 -0700

    Restored code & refactored

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Python bot for detection of spam in text messages
 
 ![GIF](https://media.giphy.com/media/QQ5WeXja47qzm/giphy.gif)
 
+![Report](./screenshots/report.png)
--- spam_bot.py
+++ spam_bot.py
@@ -1,130 +1,125 @@
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-
-
-textFile = open('./data/SMSSpamCollection','r')
-
-msgType = []
-msgBody = []
-
-testMsgType = []
-testMsgBody = []
-
-line = textFile.readline()
-count = 0
-
-neighbors = 5
-knn = KNeighborsClassifier(n_neighbors=neighbors)
-
-while line:
-    if count % 10 == 0:
-        if line.find('spam') >= 0:
-            testMsgType.append('S')
-            testMsgBody.append(line[5:len(line)])
-        elif line.find('ham') >= 0:
-            testMsgType.append('H')
-            testMsgBody.append(line[4:len(line)])
-
-    else:
-        if line.find('spam') >= 0:
-            msgType.append('S')
-            msgBody.append(line[5:len(line)])
-
-        elif line.find('ham') >= 0:
-            msgType.append('H')
-            msgBody.append(line[4:len(line)])
-
-    line = textFile.readline()
-    count += 1
-
-
-msgData = pd.DataFrame(list(zip(msgType, msgBody)), columns=['MsgType','MsgBody'])
-testMsgData = pd.DataFrame(list(zip(testMsgType, testMsgBody)), columns=['MsgType','MsgBody'])
-
-
-# print(msgData)
-# print(testMsgData)
-
-# Each test set is made up of a message type ( Ham OR Spam) & a message body (" mesgdsfsf sdf sd!.")
-# We want to test each word in testData against each word of each message in messageData to see the probability of it being Ham OR Spam...
-
-# TODO:
-#  Algorithm: HAM OR SPAM
-# 
-#   alpha = pseudo-count (we are using 0.2)
-#   N = samples (we are using 20000)
-# 
-#                        count(word,ham) + alpha
-#  P(word|ham) =   -------------------------------
-#                        count(ham) + N * alpha
-# 
-#             - - - - < OR > - - - -
-# 
-#                        count(word,spam) + alpha
-#  P(word|spam) =   -------------------------------
-#                        count(spam) + N * alpha
-# 
-# 
-# https://www.kaggle.com/dilip990/spam-ham-detection-using-naive-bayes-classifier
-
-# 
-#  What is a word? Bird is a word. 
-#  Avoid all special characters like : (?;@3$%!) except (I'll) the '
-
-
-
-# def count( word, inType, inData):
-#     count = 0
-#     # for i in range(DataFrame):    
-#     print(inData)
-#     return count
-
 import string
+from sklearn.feature_extraction.text import CountVectorizer
 
-N = 20000
-alpha = 0.2
+message = pd.read_csv('./data/SMSSpamCollection',sep='\t',names=["labels","message"])
 
-# msg = testMsgData.loc[0][1]
-# print("msg:", msg)
-# words = msg.split()
-# print("words:", words)
-# print('word:', words[0])
-# print(string.punctuation)
+test_data = []
+training_data = []
 
-# REMOVING PUNCTUATION:
-for i in range(len(msgData)):
-    mess = str(msgData.iloc[i]['MsgBody'])
+for i in range(len(message)):
+    if(i % 10 == 0):
+        test_data.append(message.iloc[i])
+    else:
+        training_data.append(message.iloc[i])
+
+pd_test_data = pd.DataFrame(test_data)
+pd_training_data = pd.DataFrame(training_data)
+
+
+# Remove Punctuation & Case for Training Set 
+for i in range(len(pd_training_data)):
+    mess = str(pd_training_data.iloc[i]['message'])
     nopunc=[char for char in mess if char not in string.punctuation]
     nopunc=''.join(nopunc)
-    msgData.iloc[i]['MsgBody'] = nopunc
+    pd_training_data.iloc[i]['message'] = nopunc.lower()
 
-for i in range(len(testMsgData)):
-    mess = str(msgData.iloc[i]['MsgBody'])
+pd_training_data.reset_index(inplace = True)
+
+# Remove Punctuation for Testing Set
+for i in range(len(pd_test_data)):
+    mess = str(pd_test_data.iloc[i]['message'])
     nopunc=[char for char in mess if char not in string.punctuation]
     nopunc=''.join(nopunc)
-    testMsgData.iloc[i]['MsgBody'] = nopunc
+    pd_test_data.iloc[i]['message'] = nopunc.lower()
+
+pd_test_data.reset_index(inplace = True)
+
+# Gather Vocab for Messages
+vocab_dictionary = {}
+for index in range(len(pd_training_data)):
+    for word in pd_training_data.iloc[index]["message"].split():
+        if not (word in vocab_dictionary):
+            vocab_dictionary[index] = word
 
 
-#  Probability of SPAM/HAM
-# 
-#                 COUNT(HAM MESSAGES) + K
-#   P(HAM) =   ----------------------------
-#                 N(TOTAL) + 1*CLASSES
+from sklearn.feature_extraction.text import CountVectorizer
 
-#  CLASSES = 2, 
+count_vector = CountVectorizer()
+count_vector.fit(pd_training_data['message'])
+count_vector.get_feature_names()
+
+doc_array = count_vector.transform(pd_training_data['message']).toarray()
+
+frequency_matrix = pd.DataFrame(doc_array, columns = count_vector.get_feature_names())
+frequency_matrix = frequency_matrix.join(pd_training_data['labels'])
+
+# Group Ham and Spam
+groups = frequency_matrix.groupby('labels')
+
+ham_features = groups.get_group("ham")
+ham_features.reset_index(drop = True, inplace = True)
+ham_features = ham_features.drop(columns = ['labels'], axis = 1)
+
+spam_features = groups.get_group("spam")
+spam_features.reset_index(drop = True, inplace = True)
+spam_features = spam_features.drop(columns = ['labels'], axis = 1)
 
 
-# Msg DATA HAM & SPAM COUNTS
-groups = msgData.groupby('MsgType')
-hamCount = groups.get_group("H").count().values[0]
-spamCount = groups.get_group("S").count().values[0]
+# key = word, value = probablity 
+# Calculate P(Ham|Word)
+ham_words = list(ham_features)
+ham_prob_dict = {}
+
+for label in ham_words:
+    feature_Total = ham_features[label].sum()
+    ham_prob_dict[label] = float(feature_Total)/ len(ham_features)
+
+spam_words = list(spam_features)
+spam_prob_dict = {}
+
+# Calculate P(Spam|Word)
+for label in spam_words:
+    feature_Total = spam_features[label].sum()
+    spam_prob_dict[label] = (feature_Total) / len(spam_features)
 
 
+predictions = []
+# Calculate the probability of Ham and Spam
+prob_ham = len(ham_features) / len(message)
+prob_spam = len(spam_features) / len(message)
+
+print("test:",ham_prob_dict['register'])
+
+for index in range(len(pd_test_data)):
+    msgProbOfHam = 1
+    msgProbOfSpam = 1
+    for word in pd_test_data.iloc[index]['message'].split():
+        if word in ham_prob_dict:
+       
+            msgProbOfHam *= ham_prob_dict[word]
+        if word in spam_prob_dict:
+            
+            msgProbOfSpam *= spam_prob_dict[word]
+
+    ham_predict = (prob_ham * msgProbOfHam) / ( (prob_ham * msgProbOfHam) + (prob_spam * msgProbOfSpam) ) 
+    spam_predict = (prob_spam * msgProbOfSpam) / ( (prob_spam * msgProbOfSpam) + (prob_ham * msgProbOfHam) )
+
+    if ham_predict > spam_predict:
+        predictions.append('ham')
+    else:
+        predictions.append('spam')
 
 
+y_true = pd_test_data["labels"].tolist()
 
-# print( len(msgData) )
-# print( msgData.iloc[len(msgData)-1][1] )
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
-# print(len(testMsgData))
+print("CONFUSION MATRIX:")
+print(confusion_matrix(y_true, predictions))
+
+print("Classification Report")
+print(classification_report(y_true, predictions))
+
